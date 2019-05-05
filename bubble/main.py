d1df12f9d462f9734ecf4c1094df2074427b2635
Chih Kai Yu <kai.chihkaiyu@gmail.com>
2019-05-05 15:40:45 +0800

    :sparkles: Add bubble sort

---
+++ bubble/main.py
@@ -0,0 +1,24 @@
+def bubble_sort(nums):
+    for i in range(len(nums)):
+        for j in range(len(nums)-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+
+
+def main():
+    input = [
+        [1, 2, 3, 4, 5],
+        [5, 4, 3, 2, 1],
+        [2, 4, 1, 3, 5],
+    ]
+
+    for i in input:
+        print('Before: {}'.format(i))
+        sorted_list = bubble_sort(i)
+        print('After: {}'.format(sorted_list))
+        print()
+
+
+if __name__ == '__main__':
+    main()
